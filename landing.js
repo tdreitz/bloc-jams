6dfd8b8b8ab7944c1fcc66cc18bac0a2c1000111
tyler <tdreitz@gmail.com>
2015-04-15 22:07:38 +0000

    Added JQuery animations to landing page

---
+++ landing.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('.hero-content h3').click(function() {
+        var subText = $(this).text();
+        $(this).text(subText + "!");
+    });
+
+    var onHoverAction = function(event) {
+      console.log('Hover Action Triggered');
+      $(this).animate({'margin-top': '-10px'});
+    };
+    var offHoverAction = function(event) {
+      console.log('Off Hover Action Triggered');
+      $(this).animate({'margin-top': '0px'});
+    };
+
+    $('.selling-points .point').hover(onHoverAction,offHoverAction);
+});
