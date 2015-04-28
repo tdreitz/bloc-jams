b963ac3ea929a5e277794522f90b3d29ed9ce50b
tyler <tdreitz@gmail.com>
2015-04-28 23:01:01 +0000

    Converted Bloc Jams to a single page application

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,38 +5,55 @@
 // require("./album");
 // require("./profile");
 
-angular.module("BlocJams", []).controller("Landing.controller", ["$scope", function($scope) {
+
   
-  $scope.heroHeader = "Bloc Jams";
+  // $scope.heroHeader = "Bloc Jams";
 
-  $scope.heroHeaderClicked = function () {
+  // $scope.heroHeaderClicked = function () {
 
-    var o = $scope.albumURLs;
+  //   var o = $scope.albumURLs;
     
-    function shuffle(o) {
-      for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-        return o;
-    };
-    var newOoo = shuffle(o);
-    $scope.albumURLs = newOoo;
-  }
+  //   function shuffle(o) {
+  //     for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+  //       return o;
+  //   };
+  //   var newOoo = shuffle(o);
+  //   $scope.albumURLs = newOoo;
+  // }
 
-  $scope.subText = "Turn the music up!";
+   // angular.module("BlocJams", []).controller("Landing.controller", ["$scope", function($scope) {}]);
 
-  $scope.subTextClicked = function() {
-    $scope.subText += " ! ";
-  }
+  blocJams = angular.module("BlocJams", ["ui.router"]);
 
-  $scope.albumURLs = [
-     '/images/album-placeholders/album-1.jpg',
-     '/images/album-placeholders/album-2.jpg',
-     '/images/album-placeholders/album-3.jpg',
-     '/images/album-placeholders/album-4.jpg',
-     '/images/album-placeholders/album-5.jpg',
-     '/images/album-placeholders/album-6.jpg',
-     '/images/album-placeholders/album-7.jpg',
-     '/images/album-placeholders/album-8.jpg',
-     '/images/album-placeholders/album-9.jpg',
-  ];
+  blocJams.config(["$stateProvider", "$locationProvider", function($stateProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+
+    $stateProvider.state("landing", {
+      url: "/",
+      controller: "Landing.controller",
+      templateUrl: "templates/landing.html"
+    });    
+  }]);
+
+  blocJams.controller("Landing.controller", ["$scope", function($scope) {
+
+      $scope.subText = "Turn the music up!";
+
+      $scope.subTextClicked = function() {
+        $scope.subText += " ! ";
+      }
+
+      $scope.albumURLs = [
+         '/images/album-placeholders/album-1.jpg',
+         '/images/album-placeholders/album-2.jpg',
+         '/images/album-placeholders/album-3.jpg',
+         '/images/album-placeholders/album-4.jpg',
+         '/images/album-placeholders/album-5.jpg',
+         '/images/album-placeholders/album-6.jpg',
+         '/images/album-placeholders/album-7.jpg',
+         '/images/album-placeholders/album-8.jpg',
+         '/images/album-placeholders/album-9.jpg',
+      ];
   
-}]);
+  }]);
+
