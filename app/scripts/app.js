8a2c127ada7c115481e1f4be96ec01b421d89493
tyler <tdreitz@gmail.com>
2015-04-30 01:36:33 +0000

    Converted album view to angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,10 +26,16 @@ blocJams.config(["$stateProvider", "$locationProvider", function($stateProvider,
     templateUrl: "templates/landing.html"
   });    
 
-  $stateProvider.state( "collection", {
+  $stateProvider.state("collection", {
     url: "/collection",
     controller: "Collection.controller",
     templateUrl: "/templates/collection.html"
+  });
+
+  $stateProvider.state("album", {
+    url: "/album",
+    templateUrl: "/templates/album.html",
+    controller: "Album.controller"
   });
 
 }]);
@@ -62,3 +68,42 @@ blocJams.controller('Collection.controller', ['$scope', function($scope){
       $scope.albums.push(angular.copy(albumPicasso));
     };
 }]);
+
+blocJams.controller('Album.controller', ['$scope', function($scope){
+  $scope.album = angular.copy(albumPicasso);
+
+  var hoveredSong = null;
+  var playingSong = null;
+
+  $scope.onHoverSong = function(song) {
+    hoveredSong = song;
+  };
+
+  $scope.offHoverSong = function(song) {
+    hoveredSong = null;
+  };
+
+  $scope.getSongState = function(song) {
+    if (song === playingSong) {
+      return "playing";
+    }
+    else if (song === hoveredSong) {
+      return "hovered";
+    }
+    return "default";
+  };
+
+  $scope.playSong = function(song) {
+    playingSong = song;
+  };
+
+  $scope.pauseSong = function(song) {
+    playingSong = null;
+  }
+
+}]);
+
+
+
+
+
