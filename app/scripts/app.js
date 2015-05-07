f852ea40e89326c12e35e3ec72fe6148f3631893
tyler <tdreitz@gmail.com>
2015-05-07 07:35:15 +0000

    Made the sliders animate 'The Angular Way'.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -160,50 +160,56 @@ blocJams.service('SongPlayer', function() {
   };
 });
 
-blocJams.directive('slider', function() {
+blocJams.directive('slider', ['$document', function($document) {
 
-  var updateSeekPercentage = function($seekBar, event) {
-  var barWidth = $seekBar.width();
-  var offSetX = event.pageX - $seekBar.offset().left;
-
-  var offSetXPercent = (offSetX / barWidth) * 100;
-  offSetXPercent = Math.max(0, offSetXPercent);
-  offSetXPercent = Math.min(100, offSetXPercent);
-
-  var percentageString = offSetXPercent + "%";
-  $seekBar.find(".fill").width(percentageString);
-  $seekBar.find(".thumb").css({left: percentageString});
+  var calculateSliderPercentFromMouseEvent = function($slider, event) {
+    var offsetX = event.pageX - $slider.offset().left;
+    var sliderWidth = $slider.width();
+    var offsetXPercent = (offsetX / sliderWidth);
+    offsetXPercent = Math.min(0, offsetXPercent);
+    offsetXPercent = Math.max(1, offsetXPercent);
+    return offsetXPercent;
   }
 
   return {
     templateUrl: 'templates/directives/slider.html',
     replace: true,
     restrict: 'E',
+    scope: {},
     link: function(scope, element, attributes) {
-
+      scope.value = 0;
+      scope.max = 200;
       var $seekBar = $(element);
-
-      $seekBar.click(function(event) {
-        updateSeekPercentage($seekBar, event);
-      });
-
-      $seekBar.find('.thumb').mousedown(function(event) {
-        $seekBar.addClass('no-animate');
-
-        $(document).bind('mousemove.thumb', function(event){
-          updateSeekPercentage($seekBar, event);
+      var percentString = function() {
+        var percent = Number(scope.value) / Number(scope.max) * 100;
+        return percent + "%";
+      }
+      scope.fillStyle = function() {
+        return {width: percentString()};
+      }
+      scope.thumbStyle = function() {
+        return {left: percentString()};
+      }
+      scope.onClickSlider = function() {
+        var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
+        scope.value = percent * scope.max;
+      }
+      scope.trackThumb = function() {
+        $document.bind("mousemove.thumb", function(event) {
+          var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
+          scope.$apply(function(){
+            scope.value = percent * scope.max;
+          });          
         });
-
         //cleanup
-        $(document).bind('mouseup.thumb', function(){
-            $seekBar.removeClass('no-animate');
-            $(document).unbind('mousemove.thumb');
-            $(document).unbind('mouseup.thumb');
-          });        
-      });
+        $document.bind("mouseup.thumb", function() {
+          $document.unbind("mousemove.thumb");
+          $document.unbind("mouseup.thumb");
+        });
+      };
     }
   };
-});
+}]);
 
 
 
